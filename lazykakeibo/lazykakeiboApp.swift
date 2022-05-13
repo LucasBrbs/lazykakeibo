aeb0d4f188f28831adfdddd9f14df7db4f8a541f
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-05-13 10:05:53 -0300

    Initial Commit

---
+++ lazykakeibo/lazykakeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazykakeiboApp.swift
+//  lazykakeibo
+//
+//  Created by Lucas Barbosa de Oliveira on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct lazykakeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
